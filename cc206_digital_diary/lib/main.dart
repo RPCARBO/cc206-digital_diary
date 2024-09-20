a6800803e163d866cca479a53184789f37da0ba3
HirokiShinsei <matthewandrei.valencia@wvsu.edu.ph>
2024-09-20 22:44:40 +0800

    For test

--- cc206_digital_diary/lib/main.dart
+++ cc206_digital_diary/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'features/diary_log-Carbo.dart';  // Import the DiaryLog widget
+import 'features/diary_log.dart';  // Import the DiaryLog widget
 
 void main() {
   runApp(const MyApp());
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
     return const MaterialApp(
       title: 'Digital Diary',
       home: DiaryLog(),  // Use DiaryLog as the home widget
