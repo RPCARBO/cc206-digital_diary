ffc8f3b505ca0a636dcfdd694e72c05c34932603
RP_Carbo <ralphpablo.carbo@wvsu.edu.ph>
2024-10-27 19:20:21 +0800

    Added new homepage.dart with login feature, using on button press trigger

--- cc206_digital_diary/lib/main.dart
+++ cc206_digital_diary/lib/main.dart
@@ -1,20 +1,23 @@
 import 'package:flutter/material.dart';
 import 'features/diary_log.dart';  // Import the DiaryLog widget
 import 'features/display_user_avatar.dart';
+import 'features/homepage.dart';  // Import the new HomePage widget
 
-void main() {
-  runApp(const MyApp());
+void main() {  
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget {  
   const MyApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-
-    return const MaterialApp(
-      title: 'Digital Diary',
-      home: DiaryLog(),  // Use DiaryLog as the home widget
-    );
+  @override  
+  Widget build(BuildContext context) {    
+    return MaterialApp(      
+      title: 'Digital Diary',      
+      home: HomePage(),      
+      routes: {        
+        '/diaryLog': (context) => DiaryLog(),      
+      },    
+    );  
   }
 }
