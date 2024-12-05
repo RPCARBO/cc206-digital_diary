43ca53efa542d51a025a156754288c2a1e034ded
HirokiShinsei <matthewandrei.valencia@wvsu.edu.ph>
2024-12-06 04:23:48 +0800

    Made the bearer token more discrete

--- diary_app/lib/backend/firebase/firebase_config.dart
+++ diary_app/lib/backend/firebase/firebase_config.dart
@@ -2,13 +2,15 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 
 
 Future<String> fetchApiKey() async {
   final response = await http.get(
     Uri.parse('https://secretmanager.googleapis.com/v1/projects/592701245398/secrets/googleapikey/versions/latest:access'),
     headers: {
-      'Authorization': 'Bearer ya29.c.c0ASRK0GaO9s0fJFnH-FOwN05FNkkyGDkhwUsL-QZq-xNzw3KpYgYbtikhweui5l7vK3yIxtCTjoyAe97nTifhqu4XTLyQ2kW1QLMY8biwCj-70WhxrHughjBDTzH4ezot3MzKxsaD8FmANlYJ5e6Q9rCpe3I-weTpXTwjqNve9L_k950zKZzUikygXWtSNw0jOSvha721v58-IqWtqc7OyRq1D_RO1G1h6h8Dhv7ucvQhWKUnzxrtjoygXWJYCbULh9d_mBtX_vRNcDkKKzmjWY2-8cgTPLLhPq9-YC9wEPseu7m8FJJuRX8qbmD4vkeNLKFgql_TydTKseg-CAyMyKCoEN1OgAMZLiIfSJnqQLVcRSCehwjNIPRgG385DfIwp0izo01BqZXxuupQSXm7p0Mb_WJxfrOe4vg55RajcJrJ95zrIhY00qdMUwrqQIhkWuJUoVmnmQFl2diFXJvc66BsJc1lcnS0mWo0lFk9uU47FkI8Uud0J53afUbxy91Bi4Sa1wJr2yY6QS2Orc0wpb95yySxrQXgubzol973sg5Wfhex0tk4YrXQyRoW_gz321gdeehWfwya6SQMXay1bf_9z6BZVpi4VugYYzUcZ4I7vJrYyu6BMsU9q5IRirwRtIkIo_WhvXgpftJ6brIBx5b2w1fkMy8tfaSZsj81vv5r_2wf08U6yX8Ve3z-Vzn2w6pzk69qtoo5JkoYY65Qdy3bflovY84qwwM-Xi810h9fnUzhJSccYIkm9-Y3whwjlm4RJSvVsfZc4qhvwpkflZYmIcfi5MexOlsMXajtBib9oi2d6wnBeq2bikwe0m03mB1_O9ByugX53WfWcUVYbvVnvOBe0Y72Ws4dfZ4SrzecXva1uhJYaYUxz5nxBuIzmwVO421_v7z0duyex_df5vd1j2vY1Rnc8zytXhhlfza4e-OXVwz1uZWinJJwyXBQWse9q3l4_98RWk8hbj55VX2nv5lrY_8sWtzoFwO5O8Bvv968mSR-UxB',
+      'Authorization': 'Bearer ${dotenv.env['BEARER_TOKEN']}',
     },
   );
 
